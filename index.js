386b4a58059f37802695425f129a30d02f765c2e
Murodbek0 <tursunaliyevmurodbek07@gmail.com>
2024-07-18 00:00:18 +0500

    examples

---
+++ index.js
@@ -0,0 +1,212 @@
+// 1 misol
+// let num = +prompt('soni krting');
+// let res = " ";
+// if(num > 0) {
+//    res = num+1;
+// }
+// console.log(res);
+
+// 2 misol
+// let num = +prompt('soni krting');
+// let res = " ";
+// if(num > 0) {
+//    res = num+1;
+// }else{
+
+//    res = num -2
+// }
+// console.log(res);
+
+// 3 misol
+
+// let num = +prompt('soni krting');
+// if(num > 0) {
+//     num++;
+// }else if(num == 0){
+//  num = 10;
+
+// }else{
+//  num =-2
+// }
+// console.log(num);
+
+// 4 misol
+// let musbat = 0;
+// let b = + prompt("son kiriting");
+// if (b > 0) {
+//     musbat++;
+// }
+// let a = + prompt("son kiriting");
+// if (a > 0) {
+//     musbat++;
+// }
+// let v = + prompt("son kiriting");
+// if (v > 0) {
+//     musbat++;
+// }
+// console.log(musbat)
+
+// 5 misol
+// let musbat = 0;
+// let manfiy = 0;
+// let b = + prompt("son kiriting");
+// if (b > 0) {
+//     musbat++;
+// } else{
+//     manfiy++;
+// }
+// let a = + prompt("son kiriting");
+// if (a > 0) {
+//     musbat++;
+
+// }  else{
+//     manfiy++;
+// }
+// let v = + prompt("son kiriting");
+// if (v > 0) {
+//     musbat++;
+
+// }  else{
+//     manfiy++;
+// }
+// console.log(musbat)
+// console.log(manfiy);
+
+// 6 misol
+// let a = Math.max(55 , 66);
+// console.log("kattasi: " + a );
+
+// 7 misol
+
+// let a = Math.min(55 , 66);
+// console.log('kichigi:' + a);
+
+// 8 misol
+// let a = Math.max(55 , 66);
+// let b = Math.min(55 , 66);
+// console.log('kattasi:' + a);
+// console.log('kichigi:' + b);
+
+// 9 misol
+// let a = 10;
+// let b = 9;
+// if(a > b){
+//   let num = a;
+//   a = b;
+//   b = num;
+
+// }
+// console.log(a);
+// console.log(b);
+
+// 10 misol
+// let a = 2;
+// let b = 5;
+// if(a ===b){
+// a = b = a +b;
+
+// }else{
+// a = b = 0;
+// }
+// console.log(a);
+// console.log(b);
+// 11 misol
+
+// 12 misol
+// let a = Math.min(88 , 66 , 77);
+// console.log('kichigi:' + a);
+
+// 13 misol
+// let u = +prompt('u');
+// let B = +prompt('B');
+// let g = +prompt('g');
+
+// let max ;
+// let norm ;
+// let min ;
+
+// if (u > B){
+// max = u,min = B} else {
+// max = B,min = u} if(max < g){
+// norm = max} else if (g < min){
+// norm = min} else{
+// norm = g}
+
+// console.log(norm);
+
+// 14 misol
+// let b = Math.min(55 , 66,95);
+// let a = Math.max(55 , 66,95);
+// console.log('kichigi:' + b);
+// console.log('kattasi:' + a);
+
+// 15 misol
+
+// 16 misol
+// let a =10;
+// let num = a > 0 ;
+// console.log(num);
+
+// 17 misol
+// let a =3;
+// let num = a % 2 ==1 ;
+// console.log(num);
+
+// 18 misol
+
+// let a =20;
+// let num = a % 2 ==0 ;
+// console.log(num);
+
+// 19 misol
+
+// let a = 5;
+// let b = 10;
+
+// let res = (a > 2) && (b >= 3);
+// console.log(res);
+
+// 20 misol
+
+// let a = 5;
+// let b = 10;
+
+// let res = (a >= 0) || (b <= 2);
+// console.log(res);
+
+// 21 misol
+
+// let a = 5;
+// let b = 6;
+// let c = 7;
+// let res = (a <= b) ||(b <=c);
+// console.log(res);
+
+// 22 misol
+// let a = 5;
+// let b = 6;
+// let c = 7;
+// let res = (a > b) && (a < c);
+// console.log(res);
+
+// 23 misol
+
+//     let a =3;
+//     let b =5;
+// let num = a % 2 ==1 ;
+// let num1 = b % 2 == 1;
+// console.log(num1);
+// console.log(num);
+
+// 24 misol
+
+// let a = 20;
+// let b = 5;
+// let res = (a % 2 !==0) || (b % 2 !==0);
+// console.log(res);
+
+// 25 misol
+// let a = 5;
+// let b = 10;
+// let res = (a % 2 ==1) && (b % 2 ==0);
+// console.log(res);
